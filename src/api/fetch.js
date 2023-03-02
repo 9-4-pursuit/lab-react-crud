0d07280d261664de00a9cf723215fffbe9c58007
BertCasale <bthebest55@gmail.com>
2023-03-01 21:37:25 -0500

    get and display all shows

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -1,3 +1,5 @@
+const URL = process.env.REACT_APP_API_BASE_URL;
+
 // Shows
 
 // Create
@@ -12,7 +14,8 @@ export function destroyShow(id) {
 
 // Index/Get all
 export function getAllShows() {
-  return;
+  return fetch(`${URL}/shows`)
+  .then((response) => response.json());
 }
 
 // Show/Get one
@@ -24,6 +27,9 @@ export function getOneShow(id) {
 export function updateShow(id, show) {
   return;
 }
+
+
+
 
 // Movies
 
