67f1c47f84699393525d9ac5469e3d5967dd4952
MaxWattan <mcwattanac@gmail.com>
2023-03-05 15:19:29 -0500

    finished lab

--- src/components/movies/MoviesEditForm.js
+++ src/components/movies/MoviesEditForm.js
@@ -1,11 +1,11 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { createShow } from "../../api/fetch";
+import { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import {updateMovie, getOneMovie} from "../../api/fetch";
+import "./MoviesForm.css"
 
-import "./ShowsForm.css";
 
-export default function ShowsForm() {
-  const [show, setShow] = useState({
+export default function MoviesEditForm() {
+  const [movie, setMovie] = useState({
     type: "",
     title: "",
     country: "",
@@ -18,20 +18,29 @@ export default function ShowsForm() {
   });
 
   let navigate = useNavigate()
+  const {id} = useParams()
 
-  function handleSubmit(e) {
-    e.preventDefault()
-    createShow(show).then((response) => {
+  useEffect(()=>{
+    getOneMovie(id).then((response)=>{
+      setMovie(response)
+    }).catch((error)=> {
+      console.log(error);
+    })
+  }, [id]);
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    updateMovie(id, movie).then((response) =>{
       console.log(response)
-      navigate(`/shows/${response.id}`)
-    }).catch((error) => {
+      navigate(`/movies/${id}`)
+    }).catch((error)=>{
       console.log(error)
     })
   }
 
   function handleTextChange(event) {
-    setShow({
-      ...show,
+    setMovie({
+      ...movie,
       [event.target.id]: event.target.value,
     });
   }
@@ -42,7 +51,7 @@ export default function ShowsForm() {
       <input
         type="text"
         id="title"
-        value={show.title}
+        value={movie.title}
         onChange={handleTextChange}
       />
 
@@ -50,7 +59,7 @@ export default function ShowsForm() {
       <input
         type="text"
         id="description"
-        value={show.description}
+        value={movie.description}
         onChange={handleTextChange}
       />
 
@@ -58,7 +67,7 @@ export default function ShowsForm() {
       <input
         type="text"
         id="type"
-        value={show.type}
+        value={movie.type}
         onChange={handleTextChange}
       />
 
@@ -66,7 +75,7 @@ export default function ShowsForm() {
       <input
         type="text"
         id="rating"
-        value={show.rating}
+        value={movie.rating}
         onChange={handleTextChange}
       />
 
@@ -74,7 +83,7 @@ export default function ShowsForm() {
       <input
         type="text"
         id="listedIn"
-        value={show.listedIn}
+        value={movie.listedIn}
         onChange={handleTextChange}
       />
 
@@ -82,7 +91,7 @@ export default function ShowsForm() {
       <input
         type="text"
         id="duration"
-        value={show.duration}
+        value={movie.duration}
         onChange={handleTextChange}
       />
 
@@ -90,7 +99,7 @@ export default function ShowsForm() {
       <input
         type="text"
         id="releaseYear"
-        value={show.releaseYear}
+        value={movie.releaseYear}
         onChange={handleTextChange}
       />
 
@@ -98,7 +107,7 @@ export default function ShowsForm() {
       <input
         type="text"
         id="country"
-        value={show.country}
+        value={movie.country}
         onChange={handleTextChange}
       />
 
@@ -106,7 +115,7 @@ export default function ShowsForm() {
       <input
         type="text"
         id="dateAdded"
-        value={show.dateAdded}
+        value={movie.dateAdded}
         onChange={handleTextChange}
       />
 
--- src/components/movies/MoviesIndex.js
+++ src/components/movies/MoviesIndex.js
@@ -1,3 +1,70 @@
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import ErrorMessage from "../errors/ErrorMessage";
+import { getAllMovies } from "../../api/fetch";
+
+import MovieListing from "./MovieListing"
+import "./MoviesIndex.css"
+
 export default function MoviesIndex() {
-  return <p>Movie List</p>;
+  const [loadingError, setLoadingError] = useState(false)
+  const [movies, setMovies] = useState([])
+  const [allMovies, setAllMovies] = useState([])
+  const [searchTitle, setSearchTitle] = useState("")
+
+  useEffect(()=>{
+    getAllMovies().then((response)=>{
+      setMovies(response)
+      setAllMovies(response)
+      setLoadingError(false)
+    }).catch((error)=>{
+      setLoadingError(true)
+    })
+  }, [])
+
+  function handleTextChange(event){
+    const title = event.target.value
+    const result = title.length ? filterMovies(title, allMovies) : allMovies
+    setSearchTitle(title)
+    setMovies(result)
+    }
+
+    function filterMovies(search, movies) {
+      return movies.filter((movie) => {
+        return movie.title.toLowerCase().match(search)
+      })
+    }
+
+  return (
+  <div>
+      {loadingError ? (
+        <ErrorMessage />
+      ) : (
+        <section className="movies-index-wrapper">
+          <h2>All Movies</h2>
+          <button>
+            <Link to="/Movies/new">Add a new movie</Link>
+          </button>
+          <br />
+          <label htmlFor="searchTitle">
+            Search Movies:
+            <input
+              type="text"
+              value={searchTitle}
+              id="searchTitle"
+              onChange={handleTextChange}
+            />
+          </label>
+          <section className="shows-index">
+            {/* <!-- MovieListing components --> */}
+            {movies.map((movie) => {
+              return <MovieListing movie={movie} key={movie.id}/>
+            })}
+          </section>
+        </section>
+      )}
+    </div>
+  );
 }
+
+
