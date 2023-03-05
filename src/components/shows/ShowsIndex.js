0ef1efc9dcb7d6c1a064ce4351d6abc42f27bf2c
jkpasaoa <jacqueline.pasaoa@gmail.com>
2023-03-04 20:10:06 -0500

    created files for movies with routes/path

--- src/components/shows/ShowsIndex.js
+++ src/components/shows/ShowsIndex.js
@@ -1,13 +1,30 @@
 import { Link } from "react-router-dom";
-
+import { useState, useEffect } from "react";
 import ErrorMessage from "../errors/ErrorMessage";
+import { getAllShows } from "../../api/fetch";
 
 import "./ShowsIndex.css";
+import ShowListing from "./ShowListing";
+
+//const URL = process.env.REACT_APP_API_BASE_URL;
+
 
 export default function ShowsIndex() {
+  const [loadingError, setLoadingError] = useState(false);
+  const[shows, setShows] = useState([]);
+
+  useEffect(() => {
+    getAllShows().then((response) => {
+      setShows(response)
+      setLoadingError(false)
+    }).catch((error) => {
+      setLoadingError(true)
+    })
+  }, [])
+
   return (
     <div>
-      {false ? (
+      {loadingError ? (
         <ErrorMessage />
       ) : (
         <section className="shows-index-wrapper">
@@ -26,6 +43,9 @@ export default function ShowsIndex() {
             />
           </label>
           <section className="shows-index">
+            {shows.map((show) => {
+              return <ShowListing show={show} key={show.id}/>
+            })}
             {/* <!-- ShowListing components --> */}
           </section>
         </section>
